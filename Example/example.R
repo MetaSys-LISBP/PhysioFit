f3d1ab2ccc1f1732f7fc3f7a5caeb5e7129e12b7
pierremillard <millard@insa-toulouse.fr>
2018-04-30 13:56:22 +0200

    Add files via upload

--- Example/example.R
+++ Example/example.R
@@ -1,32 +1,38 @@
-# 2016-08-18 millard@insa-toulouse.fr
+# 2018-04-30 millard@insa-toulouse.fr
 #
 # Calculation of growth rate and extracellular (production and consumption) fluxes, under metabolic steady-states.
 # See physio_fit.R for details.
 #
-# Copyright 2016, INRA, France
+# Copyright 2018, INRA, France
 # License: GPL v2
 
 # Reinitialize the session
 rm(list=ls(all=TRUE))
 
 # Go to PhysioFit directory
-setwd("D:/Users/millard/Desktop/Old_desktop/Drafts en cours/PhysioFit/PhysioFit")
+setwd("D:/PhysioFit/PhysioFit_1.0")
 
 # Load the main script and its dependencies
 source("physio_fit.R")
 
 # Go to the working directory
-setwd("D:/Users/millard/Desktop/Old_desktop/Drafts en cours/FucRha")
+setwd("D:/Data_to_process")
 
 # Run calculation: a simple example here, please refers to PhysioFit documentation for details
-res <- physio_fit(datfile="3b", mc=TRUE, it=100, vini=0.2,weight=c("X"=0.002,"Fuc"=0.2,"Pdiol"=0.05,"Ace"=0.02,"Lac"=1e-3,"For"=0.01))
 
-# Run in batch mode
-res_batch <- list()
-for (i in seq(4)){
-    res_batch[[i]] <- physio_fit(datfile=paste("example", i, sep=""), mc=TRUE, it=100)
-}
-lapply(res_batch, FUN = `[[`, "sens")
+# estimate first-order degradation constants
+res_deg <- estimate_k("example_blk")
+
+# estimate fluxes
+res <- physio_fit(datfile="example", mc=TRUE, it=100, vini=0.04, weight = c("DHA"=1, "X"=0.001), deg=res_deg$deg_cst)
+
+
+# # Run in batch mode
+# res_batch <- list()
+# for (i in seq(4)){
+#     res_batch[[i]] <- physio_fit(datfile=paste("example", i, sep=""), mc=TRUE, it=100)
+# }
+# lapply(res_batch, FUN = `[[`, "sens")
 
 
 
