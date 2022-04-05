18aefbd2d26aca9975a643622b0e6b741c1b6e3b
legregam <legregam@insa-toulouse.fr>
2022-04-05 16:01:02 +0200

    Fixed version handling

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-
+import physiofit
 
 # -- Project information -----------------------------------------------------
 
@@ -22,7 +22,7 @@ copyright = '2022, INRAE'
 author = 'Loïc Le Grégam, Pierre Millard'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0'
+release = physiofit.__version__
 
 
 # -- General configuration ---------------------------------------------------
