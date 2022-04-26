b620fe3e715686f9855e2fd5245e1e47bf378fa0
legregam <legregam@insa-toulouse.fr>
2022-04-26 16:58:41 +0200

    Added logic to launch CLI

--- physiofit/__main__.py
+++ physiofit/__main__.py
@@ -3,13 +3,17 @@ import sys
 import pathlib
 
 import physiofit
+import physiofit.ui.cli
 
 def main():
     """The main routine"""
 
-    path_to_app = pathlib.Path(physiofit.__path__[0])
-    path_to_app = path_to_app / "ui/gui.py"
-    run(["streamlit", "run", str(path_to_app)])
+    if len(sys.argv) > 1:
+        physiofit.ui.cli.main()
+    else:
+        path_to_app = pathlib.Path(physiofit.__path__[0])
+        path_to_app = path_to_app / "ui/gui.py"
+        run(["streamlit", "run", str(path_to_app)])
 
 if __name__ == "__main__":
     sys.exit(main())
