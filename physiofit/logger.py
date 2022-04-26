ceac7299f211bf05a6889b3f3227feede0fc048c
legregam <legregam@insa-toulouse.fr>
2022-04-26 17:04:13 +0200

    Removed adding handlers in high level logger module

--- physiofit/logger.py
+++ physiofit/logger.py
@@ -10,17 +10,18 @@ mod_logger.setLevel(logging.DEBUG)
 
 def initialize_fitter_logger(verbose):
     logger = logging.getLogger(f"PhysioFit_Logger.base.fitter.PhysioFitter")
+    logger.setLevel(logging.DEBUG)
     # fh = logging.FileHandler(f"{self.run_name}.log")
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
+    # handler = logging.StreamHandler()
+    # formatter = logging.Formatter(
+    #     '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # handler.setFormatter(formatter)
     # For debugging purposes
-    if verbose:
-        handler.setLevel(logging.DEBUG)
-    else:
-        handler.setLevel(logging.INFO)
-    if not logger.hasHandlers():
-        logger.addHandler(handler)
+    # if verbose:
+    #     handler.setLevel(logging.DEBUG)
+    # else:
+    #     handler.setLevel(logging.INFO)
+    # if not logger.hasHandlers():
+    #     logger.addHandler(handler)
 
     return logger
