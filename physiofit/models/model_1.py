a1f4734b496330e949657aecd0d5801cfd48bfd3
legregam <legregam@insa-toulouse.fr>
2024-05-14 09:38:32 +0200

    Fixed bug where t_lag wasn't actually being used in simulation. Made code clearer. Added clip on values beneath zero.

--- physiofit/models/model_1.py
+++ physiofit/models/model_1.py
@@ -13,8 +13,8 @@ class ChildModel(Model):
     def __init__(self, data):
 
         super().__init__(data)
-        self.model_name = "Steady-state batch model with lag phase and " \
-                          "degradation of metabolites "
+        self.name = "Steady-state batch model with lag phase and " \
+                          "degradation of metabolites"
         self.vini = 1
         self.parameters = None
         self.args = None
@@ -47,7 +47,7 @@ class ChildModel(Model):
 
         self.args = {"Degradation": {
             met: 0 for met in self.metabolites
-        }
+            }
         }
 
     @staticmethod
@@ -64,23 +64,33 @@ class ChildModel(Model):
         x_0, mu, t_lag = parameters[:3]
 
         # We get indices in time vector where time < t_lag
-        idx = np.nonzero(time_vector < t_lag)
+        idx = np.nonzero(time_vector < t_lag)[0]
 
         # Fill at those indices with x_0
-        x_t_lag = np.full((len(idx) - 1,), x_0)
+        x_t_lag = np.full(shape=(len(idx)), fill_value=x_0)
+
+        # Get time vector from length of t_lag to end
+        time_vector = time_vector[len(idx):]
 
         # The rest of the biomass points are calculated as usual
-        mult_by_time = x_0 * np.exp(mu * (time_vector[len(idx) - 1:] - t_lag))
+        mult_by_time = x_0 * np.exp(mu * (time_vector - t_lag))
+
+        # Concatenate both vectors and transfer to X_t column of the
+        # simulated matrix
         simulated_matrix[:, 0] = np.concatenate((x_t_lag, mult_by_time))
+
+        # Get extra arguments
         arg_values = [value for value in args["Degradation"].values()]
+
+        # optimize some calculations
+        exp_mu_t_lag = np.exp(mu * (time_vector - t_lag))
 
         for i in range(1, len(parameters) // 2):
             q, m_0 = parameters[i * 2 + 1:i * 2 + 3]
             k = arg_values[i - 1]
-            m_t_lag = np.full(len(idx) - 1, m_0)
-            mult_by_time = q * (x_0 / (mu + k)) * (
-                    np.exp(mu * (time_vector - t_lag)) - np.exp(
+            m_t_lag = np.full(len(idx), fill_value=m_0)
+            mult_by_time = q * (x_0 / (mu + k)) * (exp_mu_t_lag - np.exp(
                         -k * time_vector)) + m_0 * np.exp(-k * time_vector)
             simulated_matrix[:, i] = np.concatenate((m_t_lag, mult_by_time))
 
-        return simulated_matrix
+        return np.clip(simulated_matrix, a_min=0, a_max=None)
